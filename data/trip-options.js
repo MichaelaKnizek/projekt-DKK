452ef2551247f22ab4eb0a9091c8e15ee31d055f
Katerina <kdatsenka@gmail.com>
2025-06-07 12:12:42 +0200

    add trip-options, form component

---
+++ data/trip-options.js
@@ -0,0 +1,24 @@
+export const tripFilter = {
+  locations: ['Jizerky'],
+  difficulty: ['lehká', 'střední', 'těžká'],
+  length: ['<10', '10-15', '15-20', '20>'],
+  terrainType: [
+    'asfalt',
+    'lesní cesta',
+    'dřevěný chodník',
+    'kamenitá cesta',
+    'skály',
+    'řetězy',
+    'žebříky',
+  ],
+  features: [
+    'výhledy',
+    'vodopády',
+    'naučné stezky',
+    'občerstvení',
+    'pitná voda',
+    'přístřešky',
+    'piknik',
+    'klid',
+  ],
+};
