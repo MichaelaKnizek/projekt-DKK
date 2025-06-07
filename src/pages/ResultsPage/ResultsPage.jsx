4423cea7eb23b94d1d9c53157d1b3695f0a6fca9
Katerina <kdatsenka@gmail.com>
2025-06-07 19:37:33 +0200

    create list trips

--- src/pages/ResultsPage/ResultsPage.jsx
+++ src/pages/ResultsPage/ResultsPage.jsx
@@ -1,7 +1,20 @@
 import React from 'react';
+import { trips } from '../../../data/trasy';
 
 const ResultsPage = () => {
-  return <div>ResultsPage</div>;
+  return (
+    <div>
+      {trips.map((trip) => (
+        <div key={trip.id}>
+          <div>
+            <img src={trip.imageUrl} alt="" />
+          </div>
+          <h2>{trip.name}</h2>
+          <a href="#">Chci sem</a>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default ResultsPage;
