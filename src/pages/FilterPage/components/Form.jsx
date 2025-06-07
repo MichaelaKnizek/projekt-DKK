2aa7895355f61e25bc27ddc3424f2ad17792aa08
Katerina <kdatsenka@gmail.com>
2025-06-07 13:21:57 +0200

    change form layout - add div

--- src/pages/FilterPage/components/Form.jsx
+++ src/pages/FilterPage/components/Form.jsx
@@ -3,43 +3,55 @@ import { tripFilter } from '../../../../data/trip-options';
 import './Form.css';
 
 const Form = () => {
-  console.log(tripFilter.difficulty);
   return (
     <form>
       <label htmlFor="location">Lokalita</label>
-      <select>
-        {tripFilter.locations.map((item) => (
-          <option value={item}>{item}</option>
-        ))}
-      </select>
-      <label htmlFor="difficulty">Obtížnost</label>
-      <select>
-        {tripFilter.difficulty.map((item) => (
-          <option value={item}>{item}</option>
-        ))}
-      </select>
-      <label htmlFor="length">Délka</label>
-      <select>
-        {tripFilter.length.map((item) => (
-          <option value={item}>{item}</option>
-        ))}
-      </select>
-      <label>
-        <input />
-        Čas
-      </label>
-      <label htmlFor="terrain">Terén</label>
-      <select>
-        {tripFilter.terrainType.map((item) => (
-          <option value={item}>{item}</option>
-        ))}
-      </select>
-      <label htmlFor="features">Více možností</label>
-      <select>
-        {tripFilter.features.map((item) => (
-          <option value={item}>{item}</option>
-        ))}
-      </select>
+      <div>
+        <select>
+          {tripFilter.locations.map((item) => (
+            <option value={item}>{item}</option>
+          ))}
+        </select>
+      </div>
+      <div>
+        <label htmlFor="difficulty">Obtížnost</label>
+        <select>
+          {tripFilter.difficulty.map((item) => (
+            <option value={item}>{item}</option>
+          ))}
+        </select>
+      </div>
+      <div>
+        <label htmlFor="length">Délka</label>
+        <select>
+          {tripFilter.length.map((item) => (
+            <option value={item}>{item}</option>
+          ))}
+        </select>
+      </div>
+      <div>
+        {' '}
+        <label>
+          <input />
+          Čas
+        </label>
+      </div>
+      <div>
+        <label htmlFor="terrain">Terén</label>
+        <select>
+          {tripFilter.terrainType.map((item) => (
+            <option value={item}>{item}</option>
+          ))}
+        </select>
+      </div>
+      <div>
+        <label htmlFor="features">Více možností</label>
+        <select>
+          {tripFilter.features.map((item) => (
+            <option value={item}>{item}</option>
+          ))}
+        </select>
+      </div>
     </form>
   );
 };
