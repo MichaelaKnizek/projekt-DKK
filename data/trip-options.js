2a3584f9340dfdd2b5b4b024c0e0d650b261467a
Katerina <kdatsenka@gmail.com>
2025-06-07 16:08:22 +0200

    add handleChange function, update params

--- data/trip-options.js
+++ data/trip-options.js
@@ -1,5 +1,5 @@
 export const tripFilter = {
-  locations: ['Jizerky'],
+  locations: ['Jizerky', 'Krkonose'],
   difficulty: ['lehká', 'střední', 'těžká'],
   length: ['<10', '10-15', '15-20', '20>'],
   terrainType: [
--- src/pages/FilterPage/components/Form.jsx
+++ src/pages/FilterPage/components/Form.jsx
@@ -1,13 +1,37 @@
 import React from 'react';
 import { tripFilter } from '../../../../data/trip-options';
+import { useSearchParams } from 'react-router';
 import './Form.css';
 
 const Form = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
+  // console.log(searchParams);
+  const location = searchParams.get('location');
+  const difficulty = searchParams.get('difficulty');
+  const length = searchParams.get('length');
+
+  const updateParams = (key, value) => {
+    let params = new URLSearchParams(searchParams);
+    params.set(key, value.toString());
+    setSearchParams(params);
+  };
+  const handleChange = (e) => {
+    // console.log(e.target.value);
+    // console.log(e.target.name);
+    const key = e.target.name;
+    const { value } = e.target;
+    updateParams(key, value);
+  };
+
   return (
     <form>
       <div>
         <label htmlFor="location">Lokalita</label>
-        <select>
+        <select
+          name="location"
+          value={location}
+          onChange={(e) => handleChange(e)}
+        >
           {tripFilter.locations.map((item) => (
             <option value={item}>{item}</option>
           ))}
@@ -15,7 +39,11 @@ const Form = () => {
       </div>
       <div>
         <label htmlFor="difficulty">Obtížnost</label>
-        <select>
+        <select
+          name="difficulty"
+          value={difficulty}
+          onChange={(e) => handleChange(e)}
+        >
           {tripFilter.difficulty.map((item) => (
             <option value={item}>{item}</option>
           ))}
@@ -23,7 +51,7 @@ const Form = () => {
       </div>
       <div>
         <label htmlFor="length">Délka</label>
-        <select>
+        <select name="length" value={length} onChange={(e) => handleChange(e)}>
           {tripFilter.length.map((item) => (
             <option value={item}>{item}</option>
           ))}
@@ -39,7 +67,11 @@ const Form = () => {
         <label htmlFor="terrain">Terén</label>
         {tripFilter.terrainType.map((item) => (
           <label>
-            <input type="checkbox" name={item} />
+            <input
+              type="checkbox"
+              name={item}
+              onChange={(e) => handleChange(e)}
+            />
             {item}
           </label>
         ))}
@@ -54,7 +86,7 @@ const Form = () => {
         ))}
       </div>
       <div>
-        <label htmlFor="features">Vhodné pro</label>
+        <label htmlFor="suitableFor">Vhodné pro</label>
         {tripFilter.suitableFor.map((item) => (
           <label>
             <input type="checkbox" name={item} />
