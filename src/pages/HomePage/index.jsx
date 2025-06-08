4c1275884165b8e8c7cdd95512f0dae8b7e2cdfc
Michaela Knížková <michaela.knizek@gmail.com>
2025-06-08 11:27:56 +0200

    Add styles to HomePage

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -5,18 +5,16 @@ export const HomePage = () => {
   return (
     <div className="photo">
       <header>
-        <h1>Do kopce v klidu</h1>
+        <h1 className="title">Do kopce v klidu</h1>
       </header>
-      <main>
-        <nav className="nav__btn">
-          <NavLink to="/trips" className="btn">
-            Chci připravený výlet
-          </NavLink>
-          <NavLink to="/filter" className="btn">
-            Chci jít vlastní cestou
-          </NavLink>
-        </nav>
-      </main>
+      <nav className="navlink">
+        <NavLink to="/trips" className="btn">
+          Chci připravený výlet
+        </NavLink>
+        <NavLink to="/filter" className="btn">
+          Chci jít vlastní cestou
+        </NavLink>
+      </nav>
     </div>
   );
 };
