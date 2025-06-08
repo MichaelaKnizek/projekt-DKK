d93d87ff9dcce65caec61b3030dfbbc1b0267ca6
Michaela Knížková <michaela.knizek@gmail.com>
2025-06-08 20:55:58 +0200

    Add styles to DetailPage

--- src/pages/DetailPage/DetailPage.jsx
+++ src/pages/DetailPage/DetailPage.jsx
@@ -4,12 +4,15 @@ import React from 'react';
 
 const DetailPage = () => {
   return (
-    <div>
-      <div>fotka trasy</div>
-      <div>dlazdice s info</div>
-      <p>Popis trasy</p>
-      <p>text</p>
-      <button>SEM CHCI</button>
+    <div className="detailpage">
+      <div className="detailpage-photo">fotka trasy</div>
+      <p className="detailpage-title">Jmeno trasy</p>
+      <div>dlazdice s info - komponenta</div>
+      <div>
+        <p className="detailpage-route">Popis trasy</p>
+        <p className="detailpage-text">text</p>
+      </div>
+      <button className="detail-map">SEM CHCI</button>
     </div>
   );
 };
