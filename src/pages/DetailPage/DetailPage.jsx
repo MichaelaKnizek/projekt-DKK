3951f75e581c51773d61b3b09241070e9e5bd84d
Michaela Knížková <michaela.knizek@gmail.com>
2025-06-08 19:24:49 +0200

    Add styles to pages

--- src/pages/DetailPage/DetailPage.jsx
+++ src/pages/DetailPage/DetailPage.jsx
@@ -1,7 +1,17 @@
+import './DetailPage.css';
+
 import React from 'react';
 
 const DetailPage = () => {
-  return <div>DetailPage</div>;
+  return (
+    <div>
+      <div>fotka trasy</div>
+      <div>dlazdice s info</div>
+      <p>Popis trasy</p>
+      <p>text</p>
+      <button>SEM CHCI</button>
+    </div>
+  );
 };
 
 export default DetailPage;
