1214b65e86f8bec6480630f20198259fdf959df2
Katerina <kdatsenka@gmail.com>
2025-06-08 19:58:17 +0200

    add data tripFilter

--- data/trip-options.js
+++ data/trip-options.js
@@ -23,3 +23,34 @@ export const tripFilter = {
   ],
   suitableFor: ['kočárky', 'děti', 'psi'],
 };
+export const tripTypes = [
+  {
+    name: 'Vezmu i psa (nebo tchyni)',
+    filter: '?suitableFor=psi%2C&features=vodopády%2C',
+  },
+  { name: 'Vzhůru na piknik!', filter: '?features=piknik%2Cvýhledy%2C' },
+  { name: 'Z trasy rovnou na pivo', filter: '?features=občerstvení%2C' },
+  { name: 'Romantika pro dva', filter: '?features=výhledy%2Cklid%2C' },
+  { name: 'Rande s přírodou', filter: '?features=výhledy%2Cklid%2C' },
+  {
+    name: 'Chci si hrát (pro děti i hravé dospělé)',
+    filter:
+      '?suitableFor=děti%2Ckočárky%2C&terrain=asfalt%2C&features=naučné+stezky%2C',
+  },
+  {
+    name: 'Zahojit zlomené srdce',
+    filter: '?features=výhledy%2Cklid%2Cpřístřešky%2C',
+  },
+  {
+    name: 'Ticho, klid a žádní lidé (pro introverty)',
+    filter: '?features=výhledy%2Cklid%2C&suitableFor=psi%2C',
+  },
+  {
+    name: 'Chci fotky, které vypadají jako z Alp',
+    filter: '?features=výhledy%2Cvodopády%2C&terrain=žebříky%2C',
+  },
+  {
+    name: 'Potřebuju se vybít',
+    filter: '?terrain=skály%2Cžebříky%2Ckamenitá+cesta%2C&features=vodopády%2C',
+  },
+];
