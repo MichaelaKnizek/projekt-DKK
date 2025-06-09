cc00433a339801b3019c499bf334954bbfe5a2df
Katerina <kdatsenka@gmail.com>
2025-06-09 20:36:25 +0200

    add resultspage layout

---
+++ src/pages/ResultsPage/ResultsCard.jsx
@@ -0,0 +1,10 @@
+import { NavLink } from 'react-router';
+import './ResultsCard.css';
+
+import React from 'react';
+
+const ResultsCard = ({ name, filter, location }) => {
+  return <></>;
+};
+
+export default ResultsCard;
